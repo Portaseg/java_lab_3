1bc712ced6161adb6db5053222e56ae55247652f
PPodwojski <pietrolsek@gmail.com>
2022-01-24 00:13:31 +0100

    zad 2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,34 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        Student jeden = new Student();
+            jeden.imie = "Ania";
+            jeden.nazwisko = "Nowak";
+            jeden.wiek = 21;
+            jeden.statusStudent = true;
+
+        Student dwa = new Student();
+            dwa.imie = "Robert";
+            dwa.nazwisko = "Kowalski";
+            dwa.wiek = 25;
+            dwa.statusStudent = false;
+
+        Student trzy = new Student();
+        trzy.imie = "Kasia";
+        trzy.nazwisko = "Krol";
+        trzy.wiek = 21;
+        trzy.statusStudent = true;
+
+        Student[] studenci = {jeden, dwa, trzy};
+
+        for (Student student : studenci) {
+            String absolwent;
+            if (student.statusStudent) {
+                absolwent = "Student " + student.imie + " " + student.nazwisko + " ma aktywny status studenta.";
+            } else {
+                absolwent = "Student " + student.imie + " " + student.nazwisko +" ma nieaktywny status studenta.";
+            }
+            System.out.println(student.imie + " " + student.nazwisko + ", wiek: " + student.wiek + "\n" + absolwent + "\n");
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+   // a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
+   // klasa ma się znajdować w osobnym pliku Student.java
+        String imie;
+        String nazwisko;
+        int wiek;
+        boolean statusStudent;
+}
