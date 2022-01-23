91d94687bef0827c2844ce6c2df0a1a8e56a5cf8
PPodwojski <pietrolsek@gmail.com>
2022-01-23 23:35:49 +0100

    zad 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
+    enum LiczbyEnum{
+        JEDEN, DWA, TRZY, CZTERY, PIEC, SZESC
+    }
+
+    enum StatusEnum{
+        KONTYNUUJEMY, KONIEC
+    }
+
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
         /* ćw 1 enum-y */
 
         /* zad.1
@@ -16,6 +26,30 @@ public class Main {
         do podanych cyfr przez użytkownika,
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
+
+        while(true) {
+            System.out.println("Podaj liczbe: ");
+            int element = scan.nextInt();
+
+
+            if (element == 0) {
+                System.out.println(StatusEnum.KONIEC);
+                break;
+            } else {
+                System.out.println(StatusEnum.KONTYNUUJEMY);
+            }
+
+            switch (element) {
+                case 1 -> System.out.println(LiczbyEnum.JEDEN);
+                case 2 -> System.out.println(LiczbyEnum.DWA);
+                case 3 -> System.out.println(LiczbyEnum.TRZY);
+                case 4 -> System.out.println(LiczbyEnum.CZTERY);
+                case 5 -> System.out.println(LiczbyEnum.PIEC);
+                case 6 -> System.out.println(LiczbyEnum.SZESC);
+                default -> System.out.println("Liczba niepoprawna.");
+            }
+        }
+
 
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
